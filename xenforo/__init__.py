da64f9a972ca703d9645ecd08af03db5c9798957
Chris Hasenpflug <chris@hasenpflug.net>
2014-03-24 00:09:13 -0500

    Django 1.7 App

--- xenforo/__init__.py
+++ xenforo/__init__.py
@@ -2,3 +2,4 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+default_app_config = 'xenforo.apps.XenforoConfig'
---
+++ xenforo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class XenforoConfig(AppConfig):
+    name = 'xenforo'
+    verbose_name = "Django-XenForo"
