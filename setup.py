2fbfcb98665ea3cf1786bc317e5fdb9bdcc93996
Chris Hasenpflug <chris@hasenpflug.net>
2014-03-24 00:08:58 -0500

    Cleanup our setup.py to aline with best practices

--- setup.py
+++ setup.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(setup_requires=['d2to1'], d2to1=True)
+import versioneer
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'xenforo/_version.py'
+versioneer.versionfile_build = 'xenforo/_version.py'
+versioneer.tag_prefix = '' # tags are like 1.2.0
+versioneer.parentdir_prefix = 'xenforo-' # dirname like 'myproject-1.2.0'
+
+
+setup(
+	name = 'django-xenforo',
+	version=versioneer.get_version(),
+	description = 'A Django library to integrate with a XenForo forum.',
+
+	author = 'Chris Hasenpflug',
+	author_email = 'chris@hasenpflug.net',
+	url = 'https://bitbucket.org/chrishas35/django-xenforo/overview',
+
+	install_requires = [
+		'Django>=1.5',
+		'phpserialize',
+		'django-appconf==0.6',
+	],
+	tests_require = [
+		'pytest==2.5.1',
+		'mock',
+	],
+	test_suite = 'runtests.runtests',
+	packages=find_packages(),
+	zip_safe = False,
+	cmdclass=versioneer.get_cmdclass(),
+
+	classifiers = [
+		'Development Status :: 2 - Pre-Alpha',
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.3',
+		'Topic :: Utilities',
+		'Framework :: Django',
+	]
+)
